71635f00d6b9bc9a8238c5c90441840a48114950
Kris Zentner <krisz@microsoft.com>
2018-09-18 16:28:39 -0700

    adding some fixes

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,5 @@
 chef_gem 'azure_key_vault' do
   version node['azure_keyvault']['azure_key_vault_gem_version']
   action :install
-  compile_time true if Chef::Resource::ChefGem.instance_methods(false).include?(:compile_time)
+  compile_time true if respond_to?(:compile_time)
 end
-require 'azure_key_vault'
